6dabf293c03030ceb55959a0c89f82c6311c8541
harshsanmotra <sanmotra.harsh@gmail.com>
2024-04-27 00:23:17 -0400

    Add HW1 Files

---
+++ hw_1/task_2/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Creating build directory."
+mkdir build
+
+echo "Compiling the two objects needed for building the library **ipb_arithmetic**"
+c++ -c -I./include/ src/subtract.cpp -o build/subtract.o
+c++ -c -I./include/ src/sum.cpp -o build/sum.o
+
+echo "Making an archive of these two objects and creating first library"
+ar rcs build/libipb_arithmetic.a build/sum.o build/subtract.o
