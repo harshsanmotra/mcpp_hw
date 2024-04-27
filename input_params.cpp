8b6116b8a597122f08e42ba4c3b63bbe31119bb2
harshsanmotra <sanmotra.harsh@gmail.com>
2024-04-27 00:19:38 -0400

    Change Google Style in clang-format

--- input_params.cpp
+++ input_params.cpp
@@ -4,13 +4,13 @@ using std::cout;
 using std::endl;
 
 int main(int argc, char const *argv[]) {
-           cout << "Got " << argc << " params" << endl;
+  cout << "Got " << argc << " params" << endl;
 
-    cout << "Program " << argv[0] << endl;
+  cout << "Program " << argv[0] << endl;
 
-    for (int i = 1; i < argc; i++) {
-        cout << "argv[" << i << "]" << argv[i] << endl;
-    }
+  for (int i = 1; i < argc; i++) {
+    cout << "argv[" << i << "]" << argv[i] << endl;
+  }
 
-    return 0;
+  return 0;
 }
